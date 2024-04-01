d7495b5d8cd7a3175dbd3df155db130f6d4340f0
ariel-dev@FajardoDev <arielfajardo1991@gmail.com>
2024-03-31 23:41:07 -0400

    Second Commit

---
+++ hooks/useUser.tsx
@@ -0,0 +1,11 @@
+// git remote add origin https://github.com/FajardoDev/spotify-clone.git
+// git branch -M main
+// git push -u origin main
+
+// echo "# spotify-clone" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/FajardoDev/spotify-clone.git
+// git push -u origin main
